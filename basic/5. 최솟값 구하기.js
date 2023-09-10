3de6a38fd62a9d7296cefc9292bfded0a9e9ca1c
hyzzzzy <shinhj4068@naver.com>
2023-09-10 22:09:19 +0900

    solve: 5. 최솟값 구하기

--- basic/5. 최솟값 구하기.js
+++ basic/5. 최솟값 구하기.js
@@ -2,5 +2,14 @@ function solution(arr){
   return Math.min(...arr);
 }
 
+function solution2(arr){         
+  let answer, min=Number.MAX_SAFE_INTEGER; // 안정적인 최대 정수를 저장
+  for(let i=0; i<arr.length; i++){
+      if(arr[i]<min) min=arr[i];
+  }
+  answer=min;
+  return answer;
+}
+
 let arr = [5, 7, 1, 3, 2, 9, 11];
 console.log(solution(arr));
