52f64651847cc76f1f9e3abd9c2ccaad0c79ddb1
hyzzzzy <shinhj4068@naver.com>
2023-09-12 14:16:51 +0900

    solve: 14. 가장 긴 문자열

---
+++ basic/14. 가장 긴 문자열.js
@@ -0,0 +1,13 @@
+function solution(s){  
+  let answer = '';
+
+  for (let x of s) {
+    if (x.length > answer.length) {
+      answer = x;
+    }
+  }
+
+  return answer;
+}
+let str=["teacher", "time", "student", "beautiful", "good"];
+console.log(solution(str));
