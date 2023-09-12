da724c481da4739a608fec68fbb89f852b5d8d37
hyzzzzy <shinhj4068@naver.com>
2023-09-12 14:44:57 +0900

    solve: 16. 중복문자제거

---
+++ basic/16. 중복문자제거.js
@@ -0,0 +1,12 @@
+function solution(s){  
+  let answer = "";
+
+  for (let x of s) {
+    if (answer.indexOf(x) === - 1) {
+      answer += x;
+    }
+  }
+
+  return answer;
+}
+console.log(solution("ksekkset"));
