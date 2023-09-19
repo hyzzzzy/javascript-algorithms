937b7d85e686f5719135459306ad16aabd47d007
hyzzzzy <shinhj4068@naver.com>
2023-09-19 17:45:01 +0900

    solve: 6. 장난꾸러기 현수

---
+++ sort, greedy, deterministic-algorithm/6. 장난꾸러기 현수.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+  let answer = [];
+  let sortArr = arr.slice().sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== sortArr[i]) answer.push(i + 1);
+  }
+  return answer;
+}
+
+let arr=[120, 125, 152, 130, 135, 135, 143, 127, 160];
+console.log(solution(arr)); // 3 8
+console.log(solution([120, 130, 150, 150, 130, 150])); // 3 5
